fe2bc97515f1eaf1b2140e6145c9df65e9717a0b
mahdiabbasian <mam-1371@outlook.com>
2021-06-21 10:20:43 +0430

    initial commit

--- app/src/main/java/com/android/moviekade/di/AppModule.kt
+++ app/src/main/java/com/android/moviekade/di/AppModule.kt
@@ -20,8 +20,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -167,10 +170,27 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideMovieHouseApi(): Api =
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpClient = OkHttpClient().newBuilder()
+        okHttpClient.callTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.addInterceptor(loggingInterceptor)
+        okHttpClient.build()
+        return okHttpClient.build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideMovieHouseApi(okHttpClient: OkHttpClient): Api =
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .baseUrl(baserUrl)
+            .client(okHttpClient)
             .build()
             .create(Api::class.java)
 }
